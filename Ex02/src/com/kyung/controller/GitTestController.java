208cbe9bddd667550973eb7f4c62fea9b18bbac9
마경민 <ds1ify@gmail.com>
2016-11-17 15:07:08 +0900

    1차 테스트 스프링 따라하기..

---
+++ Ex02/src/com/kyung/controller/GitTestController.java
@@ -0,0 +1,33 @@
+package com.kyung.controller;
+
+import java.io.IOException;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GitTestController
+ */
+@WebServlet("/git.com")
+public class GitTestController extends HttpServlet implements Servlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/WEB-INF/page/gittest.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		doGet(request, response);
+	}
+
+}
